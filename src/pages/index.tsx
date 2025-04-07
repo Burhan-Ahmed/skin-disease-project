8bc296ab8c456abc431957fc0f5853e735cbdfbb
Muhammad Burhan Ahmed <92679045+Burhan-Ahmed@users.noreply.github.com>
2025-04-07 17:42:35 +0500

    Initial Setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div className="p-20 font-semibold text-2xl">
+      <main className="flex">
+        Hello Abdur Rehman, I have created the Environment for you. <br />
+        Now you can start building <br />
+        NextJs + TailwindCSS + Typescript
+      </main>
+    </div>
+  );
+}
